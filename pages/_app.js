02d34ea5ab378f36ecd691bd23ff7d299c5b48b1
Ian Austin <ianaustin2560@gmail.com>
2021-11-07 14:47:26 -0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import Navigation from "../components/navigation";
+import Footer from "../components/footer";
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" enableSystem={false}>
+    <Navigation/>
+    <Component {...pageProps} />
+    </ThemeProvider>
+    <Footer/>
+  )
 }
 
 export default MyApp
